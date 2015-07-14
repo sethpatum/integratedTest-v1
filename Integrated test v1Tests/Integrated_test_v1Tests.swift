6e4a692c7ed8b91b829cb3fbb821f0d3bd22e7f2
Seth <sethpatum@gmail.com>
2015-07-14 10:40:25 -0400

    Initial Commit

---
+++ Integrated test v1Tests/Integrated_test_v1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Integrated_test_v1Tests.swift
+//  Integrated test v1Tests
+//
+//  Created by Seth Amarasinghe on 7/14/15.
+//  Copyright (c) 2015 sunspot. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Integrated_test_v1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
