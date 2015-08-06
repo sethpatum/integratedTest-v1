69916728eb7e2f0904f71d70f5282f4908e1e04f
saman <saman@44River-saman-iMac.local>
2015-08-06 19:50:59 -0400

    rearranging + disclaimer

---
+++ Integrated test v1/PatientUIViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PatientUIViewController.swift
+//  Integrated test v1
+//
+//  Created by saman on 8/6/15.
+//  Copyright (c) 2015 sunspot. All rights reserved.
+//
+
+import UIKit
+
+var firstTimeThrough = true
+
+class PatientUIViewController: UIViewController {
+    
+    @IBAction func StartTesting(sender: AnyObject) {
+        if firstTimeThrough == true {
+            firstTimeThrough = false
+            performSegueWithIdentifier("toDisclaimer", sender: self)
+        } else {
+            performSegueWithIdentifier("toTestSelection", sender: self)
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
