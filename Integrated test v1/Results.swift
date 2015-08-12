6d3dace20991d7e2373b4c6a2ae46acad4cec4bf
saman <saman@44River-saman-iMac.local>
2015-08-12 18:10:45 -0400

    setting up for results

--- Integrated test v1/Results.swift
+++ Integrated test v1/Results.swift
@@ -8,13 +8,65 @@
 
 import UIKit
 
+var resultsArray : NSMutableArray = NSMutableArray()
+
 class Results: NSObject {
     var name:String?
     var startTime:NSDate?
     var endTime:NSDate?
+    var shortDescription:String?
+    var longDescription : NSMutableArray = NSMutableArray()
+    var screenshot : UIImage?
     
-    func output() {
+    // Return the string function to put on the header
+    func header() -> String {
+        let elapsedTime = endTime!.timeIntervalSinceDate(startTime!)
+        let duration = Int(elapsedTime)
+        return name! + " (" + String(duration) + "): " + shortDescription!
+    }
+    
+    
+    // Number of rows should be long description + one if there is a screenshot
+    func numRows() -> Int {
+        if screenshot == nil {
+            return longDescription.count
+        } else {
+            return longDescription.count + 1
+        }
+    }
+    
+    
+    // All rows are same height, except the screeshot
+    func heightForRaw(i:Int) -> Int {
+        if i < longDescription.count {
+            return 100
+        }
+        if i == longDescription.count && screenshot != nil {
+            return 1000
+        }
+        return 0
+    }
+    
+    
+    // either a text row are a screenshot raw
+    func setRow(i:Int, cell:UITableViewCell) {
+        if i < longDescription.count {
+            cell.textLabel?.text = longDescription.objectAtIndex(i) as? String
+        }
+        if i == longDescription.count && screenshot != nil {
+            cell.imageView?.image = screenshot
+        }
         
     }
+    
+    func TakeScreeshot() {
+        let layer = UIApplication.sharedApplication().keyWindow!.layer
+        let scale = UIScreen.mainScreen().scale
+        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+        
+        layer.renderInContext(UIGraphicsGetCurrentContext())
+        screenshot = UIGraphicsGetImageFromCurrentImageContext()
+    }
+    
    
 }
