e4ebc7d3272d2aaaec4d63bf00f3a3e851359fc1
Seth <sethpatum@gmail.com>
2015-11-28 13:55:08 -0500

    face name

---
+++ Integrated test v1/FaceNameAdaptiveMemoryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FaceNameAdaptiveMemoryViewController.swift
+//  CNToolkit
+//
+//  Created by Seth Amarasinghe on 11/28/15.
+//  Copyright © 2015 sunspot. All rights reserved.
+//
+
+import UIKit
+
+class FaceNameAdaptiveMemoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
