cf6cd80d5102e2e7bd92b58db1f7b6ba13c55cac
Seth <sethpatum@gmail.com>
2016-04-03 14:18:24 -0400

    2D Array re-assignments

--- Integrated test v1/FaceNameData.swift
+++ Integrated test v1/FaceNameData.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+let FemaleNames : [[String]] = [["Barbara", "Eliza", "Mary", "Jennifer", "Linda", "Patricia"],
+                                ["Sarah", "Diane", "Nancy", "Kim", "Elizabeth", "Joan"],
+                                ["Isabella", "Courtney", "Miriam", "Jane", "Dorothy", "Carol"],
+                                ["Karen", "Betty", "Jessica", "Dana", "Lisa", "Sandra"],
+                                ["Michelle", "Carol", "Amanda", "Emily", "Ashley", "Melissa"],
+                                ["Jenna",  "Caroline",  "Sofia", "Ella", "Lily", "Zoe"],
+                                ["Taylor", "Hannah", "Lauren", "Mia", "Abigail", "Alexis",]]
+
+let MaleNames : [[String]] = [["Robert", "James", "John", "Michael", "Richard", "Joseph"],
+                              ["Ralph", "Andrew", "Fred", "Henry", "Bill", "Matt"],
+                              ["Ethan", "Jeremy", "Mitchell", "Sheldon", "Doug", "Edward"],
+                              ["Chris", "Anthony", "Daniel", "Donald", "Peter", "Mark"],
+                              ["Timothy", "Brian", "Kenneth", "Ronald", "Kevin","Edward"],
+                              ["Jackson", "Samuel", "Owen", "Evan","Connor", "Nathaniel"],
+                              ["Joshua", "Ryan", "Jacob", "Jack", "Tyler", "Cameron"]]
